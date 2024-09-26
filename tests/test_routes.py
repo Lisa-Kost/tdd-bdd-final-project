31170be60e0fc3796a60e808f69ea43fe51c31c2
Lisa-Kost <lisakost1804@gmail.com>
2024-09-26 17:45:03 +0400

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -167,6 +167,15 @@ class TestProductRoutes(TestCase):
     # ADD YOUR TEST CASES HERE
     #
 
+    def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
+
     ######################################################################
     # Utility functions
     ######################################################################
@@ -178,3 +187,116 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         # logging.debug("data = %s", data)
         return len(data)
+
+    ######################################################################
+    # UPDATE AN EXISTING PRODUCT
+    ######################################################################
+
+    @app.route("/products/<int:product_id>", methods=["PUT"])
+        def update_products(product_id):
+        """
+        Update a Product
+
+        This endpoint will update a Product based the body that is posted
+        """
+        app.logger.info("Request to Update a product with id [%s]", product_id)
+        check_content_type("application/json")
+
+        product = Product.find(product_id)
+        if not product:
+            abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+        product.deserialize(request.get_json())
+        product.id = product_id
+        product.update()
+        return product.serialize(), status.HTTP_200_OK
+
+    ######################################################################
+    # LIST PRODUCTS
+    ######################################################################
+    @app.route("/products", methods=["GET"])
+        def list_products():
+            """Returns a list of Products"""
+            app.logger.info("Request to list Products...")
+
+            products = Product.all()
+
+            results = [product.serialize() for product in products]
+            app.logger.info("[%s] Products returned", len(results))
+            return results, status.HTTP_200_OK
+
+   @app.route("/products", methods=["GET"])
+        def list_products():
+            """Returns a list of Products"""
+            app.logger.info("Request to list Products...")
+
+            products = []
+            name = request.args.get("name")
+
+            if name:
+                app.logger.info("Find by name: %s", name)
+                products = Product.find_by_name(name)
+            else:
+                app.logger.info("Find all")
+                products = Product.all()
+
+            results = [product.serialize() for product in products]
+            app.logger.info("[%s] Products returned", len(results))
+            return results, status.HTTP_200_OK
+    
+    @app.route("/products", methods=["GET"])
+        def list_products():
+            """Returns a list of Products"""
+            app.logger.info("Request to list Products...")
+
+            products = []
+            name = request.args.get("name")
+            category = request.args.get("category")
+
+            if name:
+                app.logger.info("Find by name: %s", name)
+                products = Product.find_by_name(name)
+            elif category:
+                app.logger.info("Find by category: %s", category)
+                # create enum from string
+                category_value = getattr(Category, category.upper())
+                products = Product.find_by_category(category_value)
+            else:
+                app.logger.info("Find all")
+                products = Product.all()
+
+            results = [product.serialize() for product in products]
+            app.logger.info("[%s] Products returned", len(results))
+            return results, status.HTTP_200_OK
+
+    @app.route("/products", methods=["GET"])
+        def list_products():
+        """Returns a list of Products"""
+        app.logger.info("Request to list Products...")
+
+        products = []
+        name = request.args.get("name")
+        category = request.args.get("category")
+        # Get the `available` parameter from the request (hint: use `request.args.get()`
+
+        if name:
+            app.logger.info("Find by name: %s", name)
+            products = Product.find_by_name(name)
+        elif category:
+            app.logger.info("Find by category: %s", category)
+            # create enum from string
+            category_value = getattr(Category, category.upper())
+            products = Product.find_by_category(category_value)
+
+        # test to see if you received the "available" query parameter
+        # If you did, convert the available string retrieved from the query parameters to a boolean value
+        # call the Product.find_by_availability(available_value) method to retrieve products that match the specified available_value
+        # otherwise list all products
+
+        else:
+            app.logger.info("Find all")
+            products = Product.all()
+
+        results = [product.serialize() for product in products]
+        app.logger.info("[%s] Products returned", len(results))
+        return results, status.HTTP_200_OK
